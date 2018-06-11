d990a7b2a91bcd00072f0f9f4715acfcfa1d1903
Kalil Peixoto <kalilmvp@gmail.com>
2018-06-11 16:09:03 +0100

    - Creation of a Zul Proxy so the clients only know the API to be called and the proxy will redirect

--- service-registry/src/main/java/com/kmvpsolutions/stock/serviceregistry/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/kmvpsolutions/stock/serviceregistry/serviceregistry/ServiceRegistryApplication.java
@@ -3,7 +3,9 @@ package com.kmvpsolutions.stock.serviceregistry.serviceregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
