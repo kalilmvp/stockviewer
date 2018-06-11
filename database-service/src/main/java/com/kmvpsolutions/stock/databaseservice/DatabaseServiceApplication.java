70bac6eefd68b26bc764f917afce4da347893ab3
Kalil Peixoto <kalilmvp@gmail.com>
2018-06-11 14:42:56 +0100

    - Enable eureka client also to dabase-services

--- database-service/src/main/java/com/kmvpsolutions/stock/databaseservice/DatabaseServiceApplication.java
+++ database-service/src/main/java/com/kmvpsolutions/stock/databaseservice/DatabaseServiceApplication.java
@@ -2,7 +2,9 @@ package com.kmvpsolutions.stock.databaseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class DatabaseServiceApplication {
 
