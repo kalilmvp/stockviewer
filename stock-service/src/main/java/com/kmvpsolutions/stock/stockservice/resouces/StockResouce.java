2077e2140819acf69451a1fa1ed5f9076ca2a132
Kalil Peixoto <kalilmvp@gmail.com>
2018-06-11 15:32:15 +0100

    - Return of stock data from Yahoo Finance API and serve it to the client.

--- stock-service/src/main/java/com/kmvpsolutions/stock/stockservice/resouces/StockResouce.java
+++ stock-service/src/main/java/com/kmvpsolutions/stock/stockservice/resouces/StockResouce.java
@@ -13,6 +13,7 @@ import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -25,9 +26,9 @@ public class StockResouce {
     private RestTemplate restTemplate;
 
     @GetMapping("/{userName}")
-    public List<Stock> getStocksFromUsername(@PathVariable("userName") String userName) {
+    public List<Quote> getStocksFromUsername(@PathVariable("userName") String userName) {
         ResponseEntity<List<String>> quotesFromUsername = this.restTemplate.exchange(
-                "http://localhost:8300/rest/quotes/" + userName,
+                "http://database-service/rest/quotes/" + userName,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<String>>() {});
@@ -35,16 +36,42 @@ public class StockResouce {
         return this.getStocksYahoo(quotesFromUsername.getBody());
     }
 
-    private List<Stock> getStocksYahoo(List<String> quotes) {
+    private List<Quote> getStocksYahoo(List<String> quotes) {
         try {
 
             Map<String, Stock> stocks = YahooFinance.get(quotes.toArray(new String[0]));
 
-            return stocks.values().stream().collect(Collectors.toList());
+            return stocks.values().stream()
+                    .map(stock -> new Quote(stock.getName(), stock.getQuote().getSymbol(), stock.getQuote().getPrice()))
+                    .collect(Collectors.toList());
         } catch (IOException io) {
             io.printStackTrace();
         }
 
         return null;
+    }
+
+    private class Quote {
+        private final String name;
+        private final String quote;
+        private final BigDecimal price;
+
+        public Quote(String name, String quote, BigDecimal price) {
+            this.name = name;
+            this.quote = quote;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getQuote() {
+            return quote;
+        }
+
+        public BigDecimal getPrice() {
+            return price;
+        }
     }
 }
